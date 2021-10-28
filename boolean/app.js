63603362abf1b9842d7ad509261a95774eb3e47f
Đức Long <56545997+duclong09@users.noreply.github.com>
2021-10-28 14:50:40 +0700

    Update app.js

--- boolean/app.js
+++ boolean/app.js
@@ -20,3 +20,20 @@ function isComment(verifiedAt) {
     }
     return result;
 }
+
+
+
+
+/*
+
+Cho trước 2 biến a và b có các giá trị số ngẫu nhiên. Các bạn hãy giúp mình kiểm tra xem 2 biến này có bằng nhau không nhé.
+
+Kết quả của phép kiểm tra (so sánh) hãy lưu vào biến result nhé.
+*/
+
+function run(a, b) {
+    const result = (a === b) ? true : false;
+
+    return result;
+}
+
