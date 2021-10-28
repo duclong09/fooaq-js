99f9bb0a13e5aa42deece7b6877af0ffb9d49b0a
Đức Long <56545997+duclong09@users.noreply.github.com>
2021-10-28 14:58:10 +0700

    Update app.js

--- dieukien/app.js
+++ dieukien/app.js
@@ -1,1 +1,39 @@
 //if-else
+// //if-else
+// Cho biến number có thể là một số bất kì. Hãy kiểm tra number có phải là số chẵn hay không.
+
+// Nếu number là số chẵn, hãy gán 'even' cho biến result
+// Nếu number không phải số chẵn, hãy gán 'odd' cho biến result
+
+
+
+let number;
+
+let run = function(number) {
+    let result;
+    if(number % 2== 0 && number > 0){
+        result = 'even';
+    }else{
+        result = 'odd';
+    }
+    return result;
+}
+
+
+
+//Một website bán rượu chỉ cho phép người dùng truy cập nếu người dùng xác nhận đã đủ 16 tuổi. Developer phụ trách phát triển website đó đang không biết code như thế nào để kiểm tra được người dùng đã đủ tuổi chưa. Hãy giúp họ!
+
+//Cho trước biến age là số tuổi của người dùng truy cập website.
+
+
+function checkCanAccessible(age) {
+    var isAccessible;
+    
+    if(age >= 16){
+        isAccessible = true;
+    }else if( age < 16){
+        isAccessible = false;
+    }
+
+    return isAccessible;
+}
