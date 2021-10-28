66cbd49bdcda2306a0cfc06cb700216187915a50
Đức Long <56545997+duclong09@users.noreply.github.com>
2021-10-28 14:59:05 +0700

    Update app.js

--- dieukien/app.js
+++ dieukien/app.js
@@ -37,3 +37,32 @@ function checkCanAccessible(age) {
 
     return isAccessible;
 }
+
+
+
+
+//Cho trước 2 biến:
+
+//userCoin - Số tiền user có
+//bookPrice - Giá của cuốn sách
+//Hãy kiểm tra xem user có đủ tiền để mua sách không,
+//nếu đủ tiền hãy gán 'Đủ tiền' cho result, trong trường hợp không 
+//đủ tiền hãy gán 'Thiếu tiền' cho result.
+
+
+function run(userCoin, bookPrice) {
+    // Đã cho trước 2 biến userCoin, bookPrice (dòng số 3)
+    // Nên các bạn không tự khai báo 2 biến này
+
+    let result = 'Thiếu tiền'
+    
+    if(userCoin >= bookPrice){
+        result = 'Đủ tiền';
+    }else{
+        result = 'Thiếu tiền';
+    }
+
+    return result
+}
+
+
